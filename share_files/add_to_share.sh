60c71502165606adbb55d1710b017b0c9f256e46
dalong <ee_dalong@163.com>
2018-05-30 19:21:02 +0800

    modify share_files

--- share_files/add_to_share.sh
+++ share_files/add_to_share.sh
@@ -3,7 +3,8 @@ cd /home/yuanxl/dalong_research/
 git checkout share_modules
 cp $basepath/dalong_layers.py /home/yuanxl/dalong_research/share_files/
 cp $basepath/dalong_loss.py   /home/yuanxl/dalong_research/share_files/
-cp $basepath/models.py        /home/yuanxl/dalong_research/share_files/
+cp $basepath/dalong_models.py  /home/yuanxl/dalong_research/share_files/
+cp $basepath/add_to_share.sh  /home/yuanxl/dalong_research/share_files/
 git add share_files/
 git commit -m 'modify share_files'
 git push origin share_modules
